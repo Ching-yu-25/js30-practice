b91e857e0afe0bbbca28ee1b7bec66bbacd9c06c
Ching-yu-25 <ycy0227@gmail.com>
2021-09-15 21:40:01 +0800

    210915-practice update
    practice update

---
+++ vue-practice/es_member.js
@@ -0,0 +1,71 @@
+const members = [{
+  id: 1,
+  comp: 'New Dimension',
+  team: 'Knights',
+  name: 'Tsukasa Suou',
+}, {
+  id: 2,
+  comp: 'New Dimension',
+  team: 'Knights',
+  name: 'Leo Tsukinaga',
+}, {
+  id: 3,
+  comp: 'New Dimension',
+  team: 'Knights',
+  name: 'Izumi Sena',
+}, {
+  id: 4,
+  comp: 'New Dimension',
+  team: 'Knights',
+  name: 'Ritsu Sakuma',
+}, {
+  id: 5,
+  comp: 'New Dimension',
+  team: 'Knights',
+  name: 'Arashi Narukami',
+}, {
+  id: 6,
+  comp: 'New Dimension',
+  team: 'Switch',
+  name: 'Natsume Sakasaki',
+}, {
+  id: 7,
+  comp: 'New Dimension',
+  team: 'Switch',
+  name: 'Aoba Tsumuki',
+}, {
+  id: 8,
+  comp: 'New Dimension',
+  team: 'Switch',
+  name: 'Sora Harukawa',
+}, {
+  id: 9,
+  comp: 'New Dimension',
+  team: 'Double Face',
+  name: 'Madara Mikejima',
+}, {
+  id: 10,
+  comp: 'New Dimension',
+  team: 'Double Face',
+  name: 'Kohaku Oukawa',
+}, {
+  id: 11,
+  comp: 'StarMaker Production',
+  team: 'Trickstar',
+  name: 'Hokuto Hidaka',
+}, {
+  id: 12,
+  comp: 'StarMaker Production',
+  team: 'Trickstar',
+  name: 'Subaru Akehoshi',
+}, {
+  id: 13,
+  comp: 'StarMaker Production',
+  team: 'Trickstar',
+  name: 'Makoto Yuki',
+}, {
+  id: 14,
+  comp: 'StarMaker Production',
+  team: 'Trickstar',
+  name: 'Mao Isara',
+}]
